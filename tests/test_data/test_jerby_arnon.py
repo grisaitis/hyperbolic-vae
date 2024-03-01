df4ddda49da9143e46b7d65bc93876d8b7933cc6
William Grisaitis <wgrisaitis@gmail.com>
2024-03-01 16:49:39 +0000

    continue implementing jerby-arnon data module

---
+++ tests/test_data/test_jerby_arnon.py
@@ -0,0 +1,17 @@
+import tempfile
+from pathlib import Path
+
+from hyperbolic_vae.data.jerby_arnon import (
+    ANNOTATIONS_CSV_GZ_URL,
+    JerbyArnonCSVDataModule,
+    _download_and_extract_csv_gz,
+)
+
+
+def test_download_and_extract_csv_gz():
+    with tempfile.TemporaryDirectory() as tempdir:
+        save_path = Path(tempdir) / "annotations.csv"
+        _download_and_extract_csv_gz(ANNOTATIONS_CSV_GZ_URL, save_path)
+        assert save_path.exists()
+        assert save_path.is_file()
+        assert save_path.stat().st_size > 0
