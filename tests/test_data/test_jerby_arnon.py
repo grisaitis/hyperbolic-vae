34cda380303ab761a586d49537fa9271c2645d92
William Grisaitis <wgrisaitis@gmail.com>
2024-03-11 22:08:17 +0000

    add a test for jerby-arnon rna-seq data module

--- tests/test_data/test_jerby_arnon.py
+++ tests/test_data/test_jerby_arnon.py
@@ -15,3 +15,13 @@ def test_download_and_extract_csv_gz():
         assert save_path.exists()
         assert save_path.is_file()
         assert save_path.stat().st_size > 0
+
+
+def test_data_module():
+    with tempfile.TemporaryDirectory() as tempdir:
+        data_module = JerbyArnonCSVDataModule(data_path=Path(tempdir), batch_size=2, num_workers=0)
+        assert data_module.data_path.exists()
+        assert data_module.batch_size == 2
+        assert data_module.num_workers == 0
+        data_module.prepare_data()
+        data_module.setup(stage="fit")
