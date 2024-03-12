999b367c5f0e90796341fcd6436f816b1ac52e58
William Grisaitis <wgrisaitis@gmail.com>
2024-03-12 21:27:46 +0000

    add some logging to jerby-arnon dataset making

--- hyperbolic_vae/datasets/jerby_arnon.py
+++ hyperbolic_vae/datasets/jerby_arnon.py
@@ -1,6 +1,7 @@
 import gzip
 import io
 import itertools
+import logging
 import types
 import urllib
 from pathlib import Path
@@ -10,6 +11,8 @@ import pandas as pd
 import pytorch_lightning as pl
 import torch
 from torch.utils.data import Dataset
+
+logger = logging.getLogger(__name__)
 
 ANNOTATIONS_CSV_GZ_URL = (
     "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE115nnn/GSE115978/suppl/GSE115978%5Fcell.annotations.csv.gz"
@@ -58,6 +61,9 @@ class RNASeqAnnotatedDataset(Dataset):
             df_rnaseq (pd.DataFrame): A DataFrame of shape (n samples, n genes) with RNA-seq counts or transcripts.
             df_annotations (pd.DataFrame): A DataFrame of shape (n samples, annotations) with cell_type and other columns.
         """
+        assert df_rnaseq.index.name == columns.SINGLE_CELL_ID
+        assert df_rnaseq.columns.name == columns.GENE_SYMBOL
+        assert df_annotations.index.name == columns.SINGLE_CELL_ID
         assert df_rnaseq.index.equals(df_annotations.index)
         self.df_rnqseq = df_rnaseq
         self.df_annotations = df_annotations
@@ -74,10 +80,12 @@ class RNASeqAnnotatedDataset(Dataset):
 
 def _read_annotations(path_csv: Path) -> pd.DataFrame:
     """Returns a pd.DataFrame with cells as rows, annotations as columns."""
+    logger.info(f"Reading annotations from {path_csv}")
     df = pd.read_csv(
         path_csv,
         na_values={"cell.types": "?"},
     )
+    logger.info(f"Read annotations from {path_csv}")
     df = df.rename(
         columns={
             "cells": columns.SINGLE_CELL_ID,
@@ -85,29 +93,37 @@ def _read_annotations(path_csv: Path) -> pd.DataFrame:
             "samples": columns.SAMPLE_ID,
         }
     )
+    logger.info("Renamed columns")
     df = df.replace({columns.CELL_TYPE: weird_to_nice})
     df = df.rename_axis(index=columns.SINGLE_CELL_ID)
     df = df.set_index(columns.SINGLE_CELL_ID, drop=False)
     df = df.sort_index()
+    logger.info("Sorted index")
     return df
 
 
 def _read_tpm(path_csv: Path) -> pd.DataFrame:
     """Returns a pd.DataFrame with cells as rows, genes as columns."""
+    logger.info("reading TPM from %s", path_csv)
     df = pd.read_csv(path_csv, engine="pyarrow", index_col=0)
+    logger.info("renaming and sorting index")
     df = df.rename_axis(index=columns.GENE_SYMBOL, columns=columns.SINGLE_CELL_ID)
     df = df.sort_index(axis="columns")
     df = df.sort_index(axis="rows")
+    logger.info("transposing")
     df = df.T
+    logger.info("shape: %s", df.shape)
     return df
 
 
 def _filter_gene_symbols(df_tpm: pd.DataFrame) -> pd.DataFrame:
     # remove mitochondrial gene symbols
     mitochondrial_gene_symbols = df_tpm.columns[df_tpm.columns.str.startswith("MT-")]
+    logger.info("dropping %s mitochondrial gene symbols", len(mitochondrial_gene_symbols))
     df_tpm = df_tpm.drop(columns=mitochondrial_gene_symbols)
     # identify and remove gene symbols which are zero more than 90% of the time
     zero_genes = df_tpm.columns[df_tpm.eq(0).mean() > 0.9]
+    logger.info("dropping %s gene symbols which are zero more than 90%% of the time", len(zero_genes))
     df_tpm = df_tpm.drop(columns=zero_genes)
     return df_tpm
 
