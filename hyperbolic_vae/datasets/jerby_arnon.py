896d5519d14431b6685ff28805de0629faae9c69
William Grisaitis <wgrisaitis@gmail.com>
2024-03-12 20:33:17 +0000

    add script, datasets for training hyperbolic VAE on jerby-arnon RNA-seq

--- hyperbolic_vae/datasets/jerby_arnon.py
+++ hyperbolic_vae/datasets/jerby_arnon.py
@@ -8,12 +8,16 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
+import torch
+from torch.utils.data import Dataset
 
 ANNOTATIONS_CSV_GZ_URL = (
     "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE115nnn/GSE115978/suppl/GSE115978%5Fcell.annotations.csv.gz"
 )
 COUNTS_CSV_GZ_URL = "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE115nnn/GSE115978/suppl/GSE115978%5Fcounts.csv.gz"
 TPM_CSV_GZ_URL = "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE115nnn/GSE115978/suppl/GSE115978%5Ftpm.csv.gz"
+ANNOTATIONS_CSV_PATH = Path("/home/jupyter/hyperbolic-vae/data/jerby_arnon/manual/GSE115978_cell.annotations.csv")
+TPM_CSV_PATH = Path("/home/jupyter/hyperbolic-vae/data/jerby_arnon/manual/GSE115978_tpm.csv")
 
 
 columns = types.SimpleNamespace(
@@ -41,13 +45,31 @@ nice_to_weirds = {
 weird_to_nice = {weird: nice for nice in nice_to_weirds for weird in nice_to_weirds[nice]}
 
 
-def _download_and_extract_csv_gz(url: str, save_path: Path) -> None:
-    with urllib.request.urlopen(url) as response:
-        compressed_file = io.BytesIO(response.read())
-    with gzip.open(compressed_file, "rb") as gz:
-        decompressed_content = gz.read()
-    with open(save_path, "wb") as f_out:
-        f_out.write(decompressed_content)
+class RNASeqAnnotatedDataset(Dataset):
+    def __init__(
+        self,
+        df_rnaseq: pd.DataFrame,
+        df_annotations: pd.DataFrame,
+    ):
+        """
+        Initialize the RNASeqAnnotatedDataset object.
+
+        Args:
+            df_rnaseq (pd.DataFrame): A DataFrame of shape (n samples, n genes) with RNA-seq counts or transcripts.
+            df_annotations (pd.DataFrame): A DataFrame of shape (n samples, annotations) with cell_type and other columns.
+        """
+        assert df_rnaseq.index.equals(df_annotations.index)
+        self.df_rnqseq = df_rnaseq
+        self.df_annotations = df_annotations
+
+    def __len__(self):
+        return len(self.df_annotations)
+
+    def __getitem__(self, idx):
+        return {
+            "rnaseq": torch.tensor(self.df_rnqseq[idx], dtype=torch.float),
+            "annotations": torch.tensor(self.df_annotations[idx], dtype=torch.float),
+        }
 
 
 def _read_annotations(path_csv: Path) -> pd.DataFrame:
@@ -90,126 +112,8 @@ def _filter_gene_symbols(df_tpm: pd.DataFrame) -> pd.DataFrame:
     return df_tpm
 
 
-def _save_split_parquet_datasets(
-    df_annotations: pd.DataFrame,
-    df_tpm: pd.DataFrame,
-    rng: np.random.Generator,
-    parquet_datasets_path: Path,
-) -> None:
-    # sample 0.6 train, 0.2 val, 0.2 test
-    parquet_datasets_path.mkdir(exist_ok=True)
-    all_cell_ids = list(df_annotations.index)
-    all_cell_ids = rng.permutation(all_cell_ids)
-    n_train, n_val = int(0.6 * len(all_cell_ids)), int(0.2 * len(all_cell_ids))
-    n_test = len(all_cell_ids) - n_train - n_val
-    split_cell_ids = {
-        "train": all_cell_ids[:n_train],
-        "val": all_cell_ids[n_train : n_train + n_val],
-        "test": all_cell_ids[-n_test:],
-    }
-    for split, cell_ids in split_cell_ids.items():
-        df_annotations_split = df_annotations.loc[cell_ids]
-        df_tpm_split = df_tpm.loc[cell_ids]
-        df_annotations_split.to_parquet(parquet_datasets_path / f"{split}_annotations.parquet")
-        df_tpm_split.to_parquet(parquet_datasets_path / f"{split}_rnaseq.parquet")
-
-
-class JerbyArnonCSVDataModule(pl.LightningDataModule):
-    def __init__(self, data_path: Path, batch_size: int, num_workers: int):
-        super().__init__()
-        self.data_path = data_path
-        assert self.data_path.exists(), f"Path {self.data_path} does not exist."
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-
-    def prepare_data(self) -> None:
-        """
-        Prepare data by downloading, extracting, cleaning, saving splits. Called only once globally.
-        """
-        for url, csv_filename in [(ANNOTATIONS_CSV_GZ_URL, "annotations.csv"), (TPM_CSV_GZ_URL, "tpm.csv")]:
-            csv_path = self.data_path / csv_filename
-            if not csv_path.exists():
-                _download_and_extract_csv_gz(url, csv_path)
-        parquet_datasets_path = self.data_path / "parquet_datasets"
-        if not parquet_datasets_path.exists():
-            rng = np.random.default_rng(seed=42)
-            df_annotations = _read_annotations(self.data_path / "annotations.csv")
-            df_tpm = _read_tpm(self.data_path / "tpm.csv")
-            df_tpm = _filter_gene_symbols(df_tpm)
-            _save_split_parquet_datasets(df_annotations, df_tpm, rng, parquet_datasets_path)
-
-    def setup(self, stage: str) -> None:
-        """
-        Set state that is specific to each process, such as splitting data into train/validation/test sets.
-        """
-        parquet_datasets_path = self.data_path / "parquet_datasets"
-        if stage == "fit" or stage is None:
-            self.df_train_rnaseq = pd.read_parquet(parquet_datasets_path / "train_rnaseq.parquet")
-            self.df_train_annotations = pd.read_parquet(parquet_datasets_path / "train_annotations.parquet")
-            self.df_val_rnaseq = pd.read_parquet(parquet_datasets_path / "val_rnaseq.parquet")
-            self.df_val_annotations = pd.read_parquet(parquet_datasets_path / "val_annotations.parquet")
-        if stage == "test" or stage is None:
-            self.df_test_rnaseq = pd.read_parquet(parquet_datasets_path / "test_rnaseq.parquet")
-            self.df_test_annotations = pd.read_parquet(parquet_datasets_path / "test_annotations.parquet")
-
-
-# class JerbyArnonCSVDataset(torch.utils.data.Dataset):
-#     def __init__(self):
-#         """
-#         Initialize the JerbyArnonCSVDataset.
-
-#         Args:
-#             annotations (str): The URL of the gzip compressed CSV file containing annotations for each cell.
-#             counts (str): The URL of the gzip compressed CSV file containing counts for each gene symbol.
-#             tpm (str): The URL of the gzip compressed CSV file containing TPM values for each gene symbol.
-
-#         """
-#         self.annotations = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE115978&format=file&file=GSE115978%5Fcell%2Eannotations%2Ecsv%2Egz"
-#         self.counts = (
-#             "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE115978&format=file&file=GSE115978%5Fcounts%2Ecsv%2Egz"
-#         )
-#         self.tpm = (
-#             "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE115978&format=file&file=GSE115978%5Ftpm%2Ecsv%2Egz"
-#         )
-
-#         # Load the data
-#         self.annotations_data = self._load_data(self.annotations)
-#         self.counts_data = self._load_data(self.counts)
-#         self.tpm_data = self._load_data(self.tpm)
-
-#     def _load_data(self, url: str) -> pd.DataFrame:
-#         """
-#         This method is used to load data from a given URL. The data is expected to be a gzip compressed CSV file.
-
-#         Parameters:
-#         url (str): The URL of the gzip compressed CSV file.
-
-#         Returns:
-#         pd.DataFrame: The loaded data as a pandas DataFrame.
-#         """
-#         # Download and load the gzip compressed CSV file
-#         with gzip.open(url, "rt") as file:
-#             data = pd.read_csv(file, engine="pyarrow")
-
-#         return data
-
-#     def train_val_test_split(self, train_size, val_size, test_size, stratify):
-#         # Split the data into train, validation, and test sets
-#         # The split is stratified based on the given column
-#         return
-
-
-# dataset = JerbyArnonCSVDataset(
-#     )
-
-# dataset_train, dataset_val, dataset_test = dataset.train_val_test_split(
-#     train_size=0.8, val_size=0.1, test_size=0.1, stratify="time"
-# )
-
-# scrnaseq_data_module = pl.LightningDataModule.from_datasets(
-#     train_dataset=dataset_train,
-#     val_dataset=dataset_val,
-#     test_dataset=dataset_test,
-#     batch_size=256,
-#     num_workers=4,
-# )
+def get_pytorch_dataset() -> RNASeqAnnotatedDataset:
+    df_annotations = _read_annotations(ANNOTATIONS_CSV_PATH)
+    df_rnaseq = _read_tpm(TPM_CSV_PATH)
+    df_rnaseq = _filter_gene_symbols(df_rnaseq)
+    return RNASeqAnnotatedDataset(df_rnaseq, df_annotations)
