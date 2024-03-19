ba5e3384dcd7ca1c5248a39867874b7865021be0
William C Grisaitis <wgrisaitis@gmail.com>
2024-03-19 19:47:20 +0000

    update pyproject to use pytorch CPU sources, python 3.11

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,3 +1,9 @@
 curl https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/.gitalias
 git config --global include.path ~/.gitalias
-PIP_NO_DEPS=1 conda env create -f conda-env-cpu.yml -y
+# conda env create -f .devcontainer/conda-env-cpu.yml -y
+# conda env update --name base --file .devcontainer/conda-env-cpu.yml -y
+# conda activate hyperbolic-vae
+# pip install --no-deps -e .
+conda install -c conda-forge python=3.11 poetry -y
+# python -m pip install poetry
+pip install -e .
