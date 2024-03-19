e7318ae3b925d91de36350082faa154b0ebba914
William C Grisaitis <wgrisaitis@gmail.com>
2024-03-19 23:09:41 +0000

    use python 3.11 base image; change vscode extenstions; use pylance; instead of conda, use poetry to install everything; clean up settings.json

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,6 @@
 curl https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/.gitalias
 git config --global include.path ~/.gitalias
-# conda env create -f .devcontainer/conda-env-cpu.yml -y
-# conda env update --name base --file .devcontainer/conda-env-cpu.yml -y
-# conda activate hyperbolic-vae
-# pip install --no-deps -e .
-conda install -c conda-forge python=3.11 poetry -y
-# python -m pip install poetry
-pip install -e .
+python -m pip install --upgrade pip
+pip install poetry
+poetry config virtualenvs.in-project true
+poetry install --no-interaction
